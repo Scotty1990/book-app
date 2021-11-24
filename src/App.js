8c49ebde1f7de2408942a8f17581f7b6a3be7ab9
scottmacleod <t_mac3131@hotmail.com>
2021-11-24 17:19:09 -0500

    second wednesday commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@
 import './App.css';
 import BookSearch from './components/BookSearch';
 import { Link, Route, Redirect } from 'react-router-dom';
-
+import SearchForm from './components/SearchForm';
 function App() {
   return (
     <main className="App">
       My Book App
       <Link to="/randomBook"><h3>Random Book Generator</h3></Link>
       <Route path="/randomBook" exact component={BookSearch}></Route>
+      <SearchForm
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        searchString={searchString} 
+      />
     </main>
   );
 }
--- src/components/BookSearch.jsx
+++ src/components/BookSearch.jsx
@@ -1,27 +1,32 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
+import SearchForm from './SearchForm';
 
 function BookSearch(props) {
+    const [totItems, setTotItems] = useState(String(Math.floor(Math.random() * 40)))
     const searchOptions = {
         key: process.env.REACT_APP_BOOK_KEY,
         limit: 25,
         rating: 'G',
-        api: `https://https://www.googleapis.com/books/v1/volumes?q=`,
-        endpoint: '/search'
+        api: `https://www.googleapis.com/books/v1/volumes?q=subject:fantasy&startIndex=${totItems}`,
+        endpoint: '/search',
       };
-    // const [totItems, setTotItems] = useState(String(Math.floor(Math.random() * 40)))
-    const [totItems, setTotItems] = useState(0)
+    
+    // const [totItems, setTotItems] = useState(0)
     const [books, setBooks] = useState()
+    const [bookImage, setBookImage] = useState()
     const randBookResults = document.querySelector("#randBookResults")
     let maxResults = `1`;
-    const url = `https://www.googleapis.com/books/v1/volumes?q=subject:sci+fi&startIndex=${totItems}&maxResults=${maxResults}&key=`
-    
+    const url = `${searchOptions.api}&key=`
+    // const urlEndpoint = `subject:sci+fi&startIndex=${totItems}&maxResults=${maxResults}&key=  `
     const fetchBook = () => {
         fetch(url)
         .then(res => res.json())
         .then(data => {
             console.log(data)
-            setBooks(data.items[0])
+            setBooks(data.items[0].volumeInfo.title)
+            try{setBookImage(data.items[0].volumeInfo.imageLinks.thumbnail)}
+            catch{console.log("I tried")}
             // try {
             //     randBookResults.src = data.items[0].volumeInfo.imageLinks.thumbnail
             //     console.log("try is working")
@@ -34,24 +39,24 @@ function BookSearch(props) {
             
             // console.log((data.items[0].volumeInfo.averageRating))
             setTotItems(String((Math.floor(Math.random() * parseInt(data.totalItems)))))
-            console.log(`second .then ${totItems}`)
         })
         .catch(err => console.log("something went wrong...", err))
     }
     // useEffect(() => {
     //     fetchBook();
     // }, [])
-    console.log(totItems)
+    console.log(books)
     return (
         <div>
             <div>
-                <img id="randBookResults" src={books.volumeInfo.imageLinks.thumbnail} alt=""/>
-                <div id="no-image"></div>
+                {/* <img id="randBookResults" src={bookImage}/> */}
+                <div id="no-image">{books}</div>
             </div>  
             <div>
                 <button onClick={fetchBook}>Random Book!</button>
             </div> 
             <p id="title"></p>
+            <SearchForm />
         </div>  
             
         
